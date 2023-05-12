886efd22d5f9e62078716bf03660e6fae937421c
Willian <willian.amorim1@outlook.com>
2023-05-12 10:04:04 -0300

    adicionado função para abrir a nav-mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+const openMenu = document.getElementById('img-open-menu')
+const navMobile = document.getElementById('nav-mobile')
+
+openMenu.addEventListener('click', () => {
+  navMobile.style.display = 'flex'
+})
