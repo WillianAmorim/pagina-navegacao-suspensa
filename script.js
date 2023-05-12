33620a1cf3df95e264feb0c30df828cf21d7bfaf
Willian <willian.amorim1@outlook.com>
2023-05-12 10:06:15 -0300

    adicionado função para fechar o nav-mobile

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const openMenu = document.getElementById('img-open-menu')
+const closeMenu = document.getElementById('img-close-menu')
 const navMobile = document.getElementById('nav-mobile')
 
 openMenu.addEventListener('click', () => {
   navMobile.style.display = 'flex'
+})
+
+closeMenu.addEventListener('click', () => {
+  navMobile.style.display = 'none'
 })
